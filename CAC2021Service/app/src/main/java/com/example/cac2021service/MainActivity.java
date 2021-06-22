eac8bbbde5f2ccda86750e1ea4050511f0f06237
benjaminBroudy <55467400+benjaminBroudy@users.noreply.github.com>
2021-06-21 21:03:12 -0700

    Initial Commit

---
+++ CAC2021Service/app/src/main/java/com/example/cac2021service/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.cac2021service;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public final class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private Button start, stop;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        start = (Button) findViewById(R.id.buttonStart);
+        stop = (Button) findViewById(R.id.buttonStop);
+
+
+        start.setOnClickListener(this);
+        stop.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view) {
+
+        if (view==start) {
+
+            startService(new Intent(this, extension.class));
+
+        } else if (view==stop) {
+
+            stopService(new Intent(this, extension.class));
+        }
+
+    }
+}
